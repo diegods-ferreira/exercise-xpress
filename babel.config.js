75bd04aead5785027ab43efe09b18c83c34df8ac
Diego Ferreira <diegosf1705@gmail.com>
2023-11-03 18:09:58 -0300

    chore: setup TSConfig custom paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'inline-dotenv',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@/assets': './src/assets',
+            '@/components': './src/components',
+            '@/config': './src/config',
+            '@/features': './src/features',
+            '@/routes': './src/routes',
+            '@/stores': './src/stores',
+            '@/utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
