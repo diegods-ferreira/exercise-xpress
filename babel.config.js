66c746dfa9c592129c8d59b8a83902e3a057fbca
Diego Ferreira <diegosf1705@gmail.com>
2023-11-04 10:47:19 -0300

    refactor: remove module-resolver babel plugin and enable Expo's experiments.tsconfigPaths

--- babel.config.js
+++ babel.config.js
@@ -2,23 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'inline-dotenv',
-      [
-        'module-resolver',
-        {
-          root: ['.'],
-          alias: {
-            '@/assets': './src/assets',
-            '@/components': './src/components',
-            '@/config': './src/config',
-            '@/features': './src/features',
-            '@/routes': './src/routes',
-            '@/stores': './src/stores',
-            '@/utils': './src/utils',
-          },
-        },
-      ],
-    ],
+    plugins: ['inline-dotenv'],
   };
 };
