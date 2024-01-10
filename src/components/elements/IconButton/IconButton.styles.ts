72b1c5e69dd1e66b61f6f99246d46daf1c0794ff
Diego Ferreira <diegosf1705@gmail.com>
2024-01-10 20:49:12 -0300

    refactor(IconButton): it will now use Pressable when in Android and TouchableOpacity when in iOS

--- src/components/elements/IconButton/IconButton.styles.ts
+++ src/components/elements/IconButton/IconButton.styles.ts
@@ -1,11 +1,11 @@
 import styled, { css } from 'styled-components/native';
 
-type ContainerProps = {
-  isDisabled: boolean;
+export type IconButtonProps = {
+  isDisabled?: boolean;
 };
 
-export const Container = styled.TouchableOpacity<ContainerProps>(
-  ({ theme, isDisabled }) => {
+const getIconButtonStyles = ({ isDisabled = false }: IconButtonProps) =>
+  css(({ theme }) => {
     const opacity = isDisabled ? 0.75 : 1;
 
     return css`
@@ -14,5 +14,18 @@ export const Container = styled.TouchableOpacity<ContainerProps>(
       border-radius: ${theme.roundedFull}px;
       opacity: ${opacity};
     `;
-  },
-);
+  });
+
+export const ButtonIos = styled.TouchableOpacity.attrs<IconButtonProps>({
+  activeOpacity: 0.5,
+})((props) => getIconButtonStyles(props));
+
+export const ButtonAndroid = styled.Pressable.attrs<IconButtonProps>(
+  ({ theme }) => ({
+    android_ripple: {
+      color: theme.colors.ripple,
+      borderless: true,
+      radius: theme.measures['2xl'] + theme.measures['2xl'],
+    },
+  }),
+)((props) => getIconButtonStyles(props));
--- src/components/elements/IconButton/IconButton.tsx
+++ src/components/elements/IconButton/IconButton.tsx
@@ -1,32 +1,41 @@
-import { TouchableOpacityProps } from 'react-native';
+import { PropsWithChildren } from 'react';
+import { Platform } from 'react-native';
 
 import { LucideIcon } from 'lucide-react-native';
 import { useTheme } from 'styled-components/native';
 
 import * as S from './IconButton.styles';
 
-type IconButtonProps = Pick<
-  TouchableOpacityProps,
-  'onPress' | 'onLongPress'
-> & {
-  icon: LucideIcon;
-  isDisabled?: boolean;
+type ButtonBaseProps = S.IconButtonProps & {
+  onPress: () => void;
 };
+
+type IconButtonProps = ButtonBaseProps & {
+  icon: LucideIcon;
+};
+
+function ButtonBase(props: PropsWithChildren<ButtonBaseProps>) {
+  if (Platform.OS === 'android') {
+    return <S.ButtonAndroid {...props} />;
+  }
+
+  return <S.ButtonIos {...props} />;
+}
 
 export function IconButton({
   icon: Icon,
   isDisabled = false,
-  ...rest
+  onPress,
 }: IconButtonProps) {
   const theme = useTheme();
 
   return (
-    <S.Container isDisabled={isDisabled} activeOpacity={0.5} {...rest}>
+    <ButtonBase isDisabled={isDisabled} onPress={onPress}>
       <Icon
         size={theme.fontSizes.xl}
         color={theme.colors.text}
         style={{ opacity: isDisabled ? 0.75 : 1 }}
       />
-    </S.Container>
+    </ButtonBase>
   );
 }
