7ccfea248eae0d2d670df3dddc0c4befec44e4ef
Diego Ferreira <diegosf1705@gmail.com>
2024-02-04 11:00:05 -0300

    refactor(IconButton): move styles to useStyles hook

--- src/components/elements/IconButton/IconButton.styles.ts
+++ src/components/elements/IconButton/IconButton.styles.ts
@@ -1,31 +1,22 @@
-import styled, { css } from 'styled-components/native';
+import { StyleSheet, ViewStyle } from 'react-native';
 
-export type IconButtonProps = {
+import { StylesFunctionParams } from '@/types';
+
+export type IconButtonStylesProps = {
   isDisabled?: boolean;
 };
 
-const getIconButtonStyles = ({ isDisabled = false }: IconButtonProps) =>
-  css(({ theme }) => {
-    const opacity = isDisabled ? 0.75 : 1;
+export const iconButtonStyles = ({ isDisabled }: IconButtonStylesProps) => {
+  return ({ colors, measures, roundedFull }: StylesFunctionParams) => {
+    const opacity: ViewStyle['opacity'] = isDisabled ? 0.75 : 1;
 
-    return css`
-      padding: ${theme.measures.md}px;
-      background-color: ${theme.colors.bgOffset};
-      border-radius: ${theme.roundedFull}px;
-      opacity: ${opacity};
-    `;
-  });
-
-export const ButtonIos = styled.TouchableOpacity.attrs<IconButtonProps>({
-  activeOpacity: 0.5,
-})((props) => getIconButtonStyles(props));
-
-export const ButtonAndroid = styled.Pressable.attrs<IconButtonProps>(
-  ({ theme }) => ({
-    android_ripple: {
-      color: theme.colors.ripple,
-      borderless: true,
-      radius: theme.measures['2xl'] + theme.measures['2xl'],
-    },
-  }),
-)((props) => getIconButtonStyles(props));
+    return StyleSheet.create({
+      container: {
+        padding: measures.md,
+        backgroundColor: colors.bgOffset,
+        borderRadius: roundedFull,
+        opacity,
+      },
+    });
+  };
+};
