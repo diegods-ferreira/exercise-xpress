efe760cf37f3d95f243c1fdc824c3d3f0d27251a
Diego Ferreira <diegosf1705@gmail.com>
2024-02-03 22:19:07 -0300

    refactor(Button): move to useStyles hook

--- src/hooks/useStyles.ts
+++ src/hooks/useStyles.ts
@@ -5,20 +5,27 @@ import { themes } from '@/config/styles/themes';
 import { useColorSchemeStore } from '@/stores/color-scheme';
 import { StylesObject, StylesFunctionParams } from '@/types';
 
+type UseStylesReturn<TStylesObject> = {
+  styles: StylesObject<TStylesObject>;
+  colorScheme: 'light' | 'dark';
+  theme: typeof themes.light;
+};
+
 export function useStyles<TStylesObject>(
   stylesFunction: (param: StylesFunctionParams) => TStylesObject,
-): StylesObject<TStylesObject> {
+): UseStylesReturn<TStylesObject> {
   const colorScheme = useColorSchemeStore((state) => state.colorScheme);
   const insets = useSafeAreaInsets();
+  const theme = themes[colorScheme];
 
   const styles = useMemo(() => {
     const stylesObject = stylesFunction({
       edgeInsets: insets,
-      ...themes[colorScheme],
+      ...theme,
     });
 
     return stylesObject;
-  }, [colorScheme, insets, stylesFunction]);
+  }, [insets, stylesFunction, theme]);
 
-  return styles;
+  return { styles, colorScheme, theme };
 }
