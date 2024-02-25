edc1aeb6d9a852934a3ba89998500e64dedd5e79
Diego Ferreira <diegosf1705@gmail.com>
2024-02-25 12:57:15 -0300

    refactor(constants): prepare exercises list for translation

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,9 +1,2 @@
-import { EXERCISES_LIST_EN } from './exercises-list-EN';
-import { EXERCISES_LIST_PT_BR } from './exercises-list-PT_BR';
-
-export const EXERCISES_LIST = {
-  en_US: EXERCISES_LIST_EN,
-  pt_BR: EXERCISES_LIST_PT_BR,
-} as const;
-
+export * from './exercises-list';
 export * from './workouts';
